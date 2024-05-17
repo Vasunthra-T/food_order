00a3fcdac63b482070848a9263141658a9f003b4
vasunthra-20878 <vasunthra.t@zohocorp.com>
2024-05-17 14:46:50 +0530

    host changes

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ sequelize.sync({ alter: true }) // Use force:true to drop existing tables and re
   .then(() => {
     console.log('Database synced successfully');
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
